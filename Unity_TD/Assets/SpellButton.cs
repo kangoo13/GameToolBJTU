558d9e6956716657002ad0fbde55d661e21c567b
aurelienschiltz <aurelien.schiltz@epitech.eu>
2016-04-16 20:15:23 +0800

    ok

---
+++ Unity_TD/Assets/SpellButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpellButton : MonoBehaviour {
+
+	private ISpell currentSpell;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void doSpell()
+	{
+		currentSpell.doAction ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentSpell = TestPlayer.Instance.spellActive;
+		this.transform.GetChild(0).GetComponent<Image> ().sprite = currentSpell.imageSpell;
+	}
+}
