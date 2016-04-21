d27c1555d1ebef9b14b77e05f8f15091ec395691
aurelienschiltz <aurelien.schiltz@epitech.eu>
2016-04-21 20:19:11 +0800

    wtf

--- Unity_TD/Assets/Scripts/Spell/ISpell.cs
+++ Unity_TD/Assets/Scripts/Spell/ISpell.cs
@@ -6,6 +6,8 @@ interface ISpell {
 	void doAction(Vector3 mousePosition);
 	string getTexturePath();
 	string getPrefabAnimPath();
+	string getSpellName();
+	string getSpellPrefabPath ();
 	Sprite imageSpell { get; set; }
 	GameObject prefabAnim { get; set; }
 	float initialDamage { get; set; }
