41d715a9b94a8b8886712d084af52f60894439a9
tan_d <beau-oudong.tan>
2016-04-21 22:39:44 +0800

    copie level-2

---
+++ Unity_TD/Assets/Scripts/MainMenu/CanvasScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RectTransform rt = GetComponent<RectTransform> ();
+		rt.sizeDelta = new Vector2 (800f, rt.rect.height);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
