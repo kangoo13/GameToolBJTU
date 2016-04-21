5b159e0f39821f4bc9b219084fc45388387e1317
aurelienschiltz <aurelien.schiltz@epitech.eu>
2016-04-22 03:35:16 +0800

    Sizes ok for UI

--- Unity_TD/Assets/AdjustPositionMainMenu.cs
+++ Unity_TD/Assets/AdjustPositionMainMenu.cs
@@ -5,11 +5,22 @@ public class AdjustPositionMainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-		Vector2 thisPos = GetComponent<RectTransform> ().sizeDelta;
+		RectTransform thisRt = GetComponent<RectTransform> ();
+		Vector2 thisPos = thisRt.sizeDelta;
 		RectTransform topRt = GameObject.Find("Canvas/TopPanel").GetComponent<RectTransform>();
-		Debug.Log (thisPos.x);
-		topRt.position = new Vector3 (thisPos.x / 1.96987951807f, topRt.position.y, topRt.position.z);
+		topRt.sizeDelta = new Vector2 (thisPos.x / 1.21787709497f, thisPos.y/1.376f);
+
+		RectTransform levelSelectRt = GameObject.Find ("Canvas/TopPanel/levelSelect").GetComponent<RectTransform> ();
+		levelSelectRt.sizeDelta = new Vector2 (thisPos.x / 9.08333333333f, thisPos.y/12.2857142857f);
+		levelSelectRt.anchoredPosition = new Vector2(0f, thisPos.y / 4f);
+
+
+		RectTransform scrollViewRt = GameObject.Find("Canvas/TopPanel/Scroll View").GetComponent<RectTransform>();
+		scrollViewRt.anchoredPosition = new Vector2 (0f, thisPos.y / -19.1111111111f);
+		scrollViewRt.sizeDelta = new Vector2 (topRt.sizeDelta.x, thisPos.y / 2.86666666667f);
+
+		RectTransform botRt = GameObject.Find ("Canvas/BottomPanel").GetComponent<RectTransform> ();
+		//botRt.sizeDelta = new Vector2 (topRt.sizeDelta.x, thisPos.y / 4.52631578947f);
 	}
 
 }
