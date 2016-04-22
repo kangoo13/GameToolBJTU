4d63f0fac15d5dac12bc691223cdcd2b6575f22b
tan_d <beau-oudong.tan>
2016-04-22 18:58:13 +0800

    maj

--- Unity_TD/Assets/LevelSelection.cs
+++ Unity_TD/Assets/LevelSelection.cs
@@ -8,4 +8,11 @@ public class LevelSelection : MonoBehaviour {
 	public void displayLevelSelection() {
 		topMenuObj.GetComponent<TopMainMenuHandle> ().displayLevelSelection ();
 	}
+
+	void Start() {
+		RectTransform rt = GetComponentInParent<AdjustPositionMainMenu> ().gameObject.GetComponent<RectTransform> ();
+		RectTransform thisRt = GetComponent<RectTransform> ();
+
+		thisRt.sizeDelta = new Vector2 (rt.sizeDelta.x / 7.68f, rt.sizeDelta.y / 4.51f);
+	}
 }
